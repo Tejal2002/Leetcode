cb7faa3c94dc11b5d6ad49ecb8ef7b3da2d286ea
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-23 14:57:22 +0530

    Time: 12 ms (80.20%) | Memory: 11.6 MB (58.64%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -5,14 +5,27 @@ class Solution(object):
         :rtype: int
         """
 
+    # Recursive approach
+        # if n == 0:
+        #     return 0
+        # elif n == 1:
+        #     return 1
+        # else:
+        #     return self.fib(n-1) + self.fib(n-2)
+
+    # Iterative approach 
+
         if n == 0:
             return 0
         elif n == 1:
             return 1
         else:
-            return self.fib(n-1) + self.fib(n-2)
+            a,b = 0,1
 
-         
+            for i in range(2, n+1):
+                a, b = b, a+b
+            
+            return b
         
 
         
