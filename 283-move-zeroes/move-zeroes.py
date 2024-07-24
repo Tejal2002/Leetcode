05c137764df84f1099d463b22bc2fd8795668b0d
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-24 23:49:07 +0530

    Time: 108 ms (90.71%) | Memory: 13 MB (7.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -7,14 +7,26 @@ class Solution(object):
 
     #first attempt 
 
-        count = 0
-        for i in range(len(nums)):
+        # count = 0
+        # for i in range(len(nums)):
 
-            if len(nums) > 1:
-                if nums[i] != 0:
-                    nums[count] = nums[i]
-                    if count != i:
-                        nums[i] = 0
-                    count += 1
+        #     if len(nums) > 1:
+        #         if nums[i] != 0:
+        #             nums[count] = nums[i]
+        #             if count != i:
+        #                 nums[i] = 0
+        #             count += 1
+        # return nums 
+
+    #second attempt
+        count = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
         
-        return nums 
+        for i in range(count,len(nums)):
+            nums[i] = 0
+
+        
