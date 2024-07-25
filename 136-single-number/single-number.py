d1a216ff5632d486ed0fc66ba7d7332eb8997652
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-25 22:13:19 +0530

    Time: 97 ms (53.14%) | Memory: 13.7 MB (86.23%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -5,14 +5,20 @@ class Solution(object):
         :rtype: int
         """
 
-    #First
-        n = len(nums)
-        freq = {}
+    #First , time = O(n logm) , m = size of the map
+        # n = len(nums)
+        # freq = {}
 
-        for i in range(n):
-            freq[nums[i]] = freq.get(nums[i],0) + 1
+        # for i in range(n):
+        #     freq[nums[i]] = freq.get(nums[i],0) + 1
         
-        for key in freq:
-            if freq[key] == 1:
-                return key
+        # for key in freq:
+        #     if freq[key] == 1:
+        #         return key
+    
+    #xor 
+        xor  = 0
+        for num in nums:
+            xor ^= num
         
+        return xor
