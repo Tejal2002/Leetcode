784f006237143d184f9318cd4aeb127ee3dd62cd
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-25 14:36:23 +0530

    Time: 119 ms (46.63%) | Memory: 12.9 MB (53.72%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -19,14 +19,35 @@ class Solution(object):
         # return nums 
 
     #second attempt
-        count = 0
+        # count = 0
+
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         nums[count] = nums[i]
+        #         count += 1
+        
+        # for i in range(count,len(nums)):
+        #     nums[i] = 0
+
+    # 2 pointer approach 
+
+        j = -1
 
         for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[count] = nums[i]
-                count += 1
+            if nums[i]==0:
+                j = i
+                break
         
-        for i in range(count,len(nums)):
-            nums[i] = 0
+        # if  no zero found 
+        if j == -1:
+            return nums
 
-        
+        for i in range(j+1,len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+
+        return nums
+            
+
+            
