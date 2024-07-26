ceb6ff335ecd487274d4bcb8f01c1d8e465aa1f9
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-26 21:49:25 +0530

    Time: 26 ms (13.81%) | Memory: 11.5 MB (63.07%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        
+    #first 
+        # result  = []
+
+        # for i in range(len(candies)):
+        #     flag = 1
+        #     total = candies[i] + extraCandies
+        #     for j in range(len(candies)):
+        #         if total < candies[j]:
+        #             flag = 0
+            
+        #     if flag == 0:
+        #         result.append(False)
+        #     else:
+        #         result.append(True)
+            
+        # return result  
+
+    #Second
+        result = []
+        max_candy = max(candies)
+
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= max_candy:
+                result.append(True)
+            else:
+                result.append(False)
+            
+        return result
