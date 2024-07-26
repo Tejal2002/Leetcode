d26602a5d918c5d9e027468d93db17a964fb6a0f
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-26 21:50:20 +0530

    Time: 36 ms (5.09%) | Memory: 11.4 MB (90.11%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -7,30 +7,30 @@ class Solution(object):
         """
         
     #first 
-        # result  = []
-
-        # for i in range(len(candies)):
-        #     flag = 1
-        #     total = candies[i] + extraCandies
-        #     for j in range(len(candies)):
-        #         if total < candies[j]:
-        #             flag = 0
-            
-        #     if flag == 0:
-        #         result.append(False)
-        #     else:
-        #         result.append(True)
-            
-        # return result  
-
-    #Second
-        result = []
-        max_candy = max(candies)
+        result  = []
 
         for i in range(len(candies)):
-            if candies[i] + extraCandies >= max_candy:
-                result.append(True)
-            else:
-                result.append(False)
+            flag = 1
+            total = candies[i] + extraCandies
+            for j in range(len(candies)):
+                if total < candies[j]:
+                    flag = 0
             
-        return result
+            if flag == 0:
+                result.append(False)
+            else:
+                result.append(True)
+            
+        return result  
+
+    #Second
+        # result = []
+        # max_candy = max(candies)
+
+        # for i in range(len(candies)):
+        #     if candies[i] + extraCandies >= max_candy:
+        #         result.append(True)
+        #     else:
+        #         result.append(False)
+            
+        # return result
