a3759a13c151b1a936d8f531c68c0b7bd61f1715
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-07-27 13:20:17 +0530

    Time: 14 ms (71.60%) | Memory: 12 MB (20.40%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -4,11 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # Time = O(N)
+
+        # word = s.split()
+        # rev_words = word[::-1]
+        # rev_string = ' '.join(rev_words)
+        # return rev_string
+
+        # Optimal using array 
+
+        i=0
+        length = len(s)
+        word_pos = []  #to store start and end indices of words 
+
+        while i<length:
+            
+            #skip leading spaces , vvvimp
+            while i<length and s[i] == ' ':
+                i += 1
+            if i == length:
+                break
+            start = i
+
+            while i<length and s[i] != ' ':
+                i += 1
+            end = i-1
+
+            word_pos.append((start, end))
         
-        word = s.split()
+        result = []
 
-        rev_words = word[::-1]
+        for start, end in reversed(word_pos):
+            word = s[start:end+1]
+            result.append(word)
 
-        rev_string = ' '.join(rev_words)
-
-        return rev_string
+        return ' '.join(result)
