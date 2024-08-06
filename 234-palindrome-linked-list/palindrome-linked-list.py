b5ce3373955632f1566f94d399fd97be7a944f97
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-08-06 23:29:01 +0530

    Time: 631 ms (93.02%) | Memory: 65.3 MB (78.74%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -10,20 +10,48 @@ class Solution(object):
         :rtype: bool
         """
 
-    # using arrays
+    # using arrays but space = O(n)
 
-        nums = []
+        # nums = []
 
-        while head:
-            nums.append(head.val)
-            head = head.next
+        # while head:
+        #     nums.append(head.val)
+        #     head = head.next
         
-        l,r = 0 , len(nums) - 1
+        # l,r = 0 , len(nums) - 1
 
-        while l <= r:
-            if nums[l] != nums[r]:
-                return False
-            l += 1
-            r -= 1
+        # while l <= r:
+        #     if nums[l] != nums[r]:
+        #         return False
+        #     l += 1
+        #     r -= 1
+        # return True
+    
+    # By using slow - fast pointer and reversing the second half
+
+        # Get the middle 
+        slow , fast = head , head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #reverse the second half
+        prev = None
+        curr = slow 
+        while curr:
+            tmp = curr.next
+            curr.next = prev 
+            prev = curr
+            curr = tmp
+        
+        # check palindrome
+        left , right = head , prev 
+
+        while right:
+            if left.val != right.val:
+                return False 
+            left = left.next
+            right = right.next
         return True
+
         
