07c498174779943055c5a1bdb4d5a069732943d6
Tejal2002 <96789914+Tejal2002@users.noreply.github.com>
2024-08-13 12:34:18 +0530

    Time: 171 ms (91.41%) | Memory: 12.9 MB (18.51%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -6,17 +6,32 @@ class Solution(object):
         :rtype: int
         """
     
-    # first , time =
-        l, r = 0 , len(nums)-1
+    # first , time = log(n)
+        # l, r = 0 , len(nums)-1
 
-        while l<=r :
-            m = (l+r)//2
+        # while l<=r :
+        #     m = (l+r)//2
 
-            if nums[m] < target :
-                l = m+1
-            elif nums[m] > target :
-                r = m-1
-            else:
-                return m 
+        #     if nums[m] < target :
+        #         l = m+1
+        #     elif nums[m] > target :
+        #         r = m-1
+        #     else:
+        #         return m 
         
+        # return -1
+
+    #2nd time (Iterative solution)
+        low , high = 0 , len(nums) - 1
+
+        while low <= high :
+            mid  = (low + high) // 2
+
+            if nums[mid] == target :
+                return mid 
+            elif target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
         return -1
+
